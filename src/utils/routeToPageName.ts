26dd406d8942103465596589f7b6ee82696dbd74
abrown5421 <augustashtonbrown@gmail.com>
2025-04-06 10:02:08 -0400

    fixing the casing for the crawl route in our animating function

--- src/utils/routeToPageName.ts
+++ src/utils/routeToPageName.ts
@@ -3,7 +3,7 @@ export const routeToPageName = (pathname: string): string => {
     if (pathname === "/Login") return "Auth";
     if (pathname === "/Signup") return "Auth";
     if (pathname === "/Dashboard") return "Dashboard";
-    if (/^\/crawl\/.+/.test(pathname)) return "Crawl";
+    if (/^\/Crawl\/.+/.test(pathname)) return "Crawl";
   
     return "Root"; 
 };
