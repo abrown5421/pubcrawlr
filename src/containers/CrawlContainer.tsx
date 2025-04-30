e3b8c9babdbaeaa683702d6848a1e97c259a01de
abrown5421 <augustashtonbrown@gmail.com>
2025-04-30 17:57:39 -0400

    adding in some further restricition measures to the crawl page to keep un registered users from seeing bar crawls

--- src/containers/CrawlContainer.tsx
+++ src/containers/CrawlContainer.tsx
@@ -14,6 +14,9 @@ import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import BarCard from "../components/BarCard";
 import BarCrawlBuilder from "../components/BarCrawlBuilder";
+import { useNavigate } from "react-router-dom";
+import { setActivePage } from "../store/slices/activePageSlice";
+import { useDispatch } from "react-redux";
 
 const useCrawlPageStyles = (theme: any) => ({
     logo: {
@@ -45,9 +48,12 @@ const useCrawlPageStyles = (theme: any) => ({
 });
 
 const CrawlContainer: React.FC<CrawlContainerProps> = ({ mode }) => {
+    const navigate = useNavigate();
     const theme = useTheme();
+    const dispatch = useDispatch();
     const styles = useCrawlPageStyles(theme);
     const token = useAppSelector(state => state.authentication.token);
+    const isLoggedIn = useAppSelector(state => state.authentication.isAuthenticated);
     const crawl = useAppSelector(state => state.selectedBarCrawl.selectedBarCrawl);
     const mapContainerRef = useRef<HTMLDivElement>(null);
     const ownedCrawl = mode === 'owner';
@@ -84,60 +90,99 @@ const CrawlContainer: React.FC<CrawlContainerProps> = ({ mode }) => {
         };
     }, [crawl?.centerLocation]);
     
+    const handleButtonClick = (path: string, pageName: string) => () => {
+        dispatch(setActivePage({ key: "In", value: false }));
+        dispatch(setActivePage({ key: "Name", value: pageName }));
+    
+        setTimeout(() => {
+          dispatch(setActivePage({ key: "In", value: true }));
+          navigate(path);
+        }, 500);
+    };
+
     return (
       <Box className="app-flex app-row">
-        {(crawl?.intimacyLevel ===  'Private' && isInvited) || crawl?.intimacyLevel === 'Public' ? (
+        {isLoggedIn ? (
             <>
-                <div className='app-flex app-col app-overflow-scroll app-fl-3 map-controller'>
-                    {ownedCrawl ? (
-                        <BarCrawlBuilder
-                            open={true} 
-                            onClose={() => {}} 
-                            drawerWidth={400}
-                            locationCoords={{}}
-                            mode="crawlBeingViewed"
-                        />
-                    ) : (
-                        <>
-                            <Typography sx={styles.logo} variant="h5">
-                            {crawl?.crawlName}
-                            </Typography>
-                            <Typography className="center-row" variant="body2" color="text.secondary" >
-                            <CalendarMonthIcon sx={{mr: 1}} /> {formattedStartDate} - {formattedEndDate}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" className="center-row">
-                            <GroupsIcon sx={{mr: 1}} /> {attendeesCount === 1 ? `${attendeesCount} person going` : `${attendeesCount} people going`}
-                            </Typography>
-                            <Typography className="intimacy center-row" variant="body2" color="text.secondary" >
-                            {crawl?.intimacyLevel === 'Public' ? <PublicIcon sx={{mr: 1}} /> : <AdminPanelSettingsIcon sx={{mr: 1}} />}{crawl?.intimacyLevel}
-                            </Typography>
-                            <Divider sx={{mt: 2}} />
-                            {crawl?.selectedBars.map((bar, index) => (
-                                <BarCard key={index} bar={bar} mode="viewing" />
-                            ))}
-                        </>
-                    )}
-                </div>
-                <div className='app-flex app-col app-relative app-overflow-hidden app-h-percent-100 app-fl-8 map-container' ref={mapContainerRef} />
+                {(crawl?.intimacyLevel ===  'Private' && isInvited) || crawl?.intimacyLevel === 'Public' ? (
+                    <>
+                        <div className='app-flex app-col app-overflow-scroll app-fl-3 map-controller'>
+                            {ownedCrawl ? (
+                                <BarCrawlBuilder
+                                    open={true} 
+                                    onClose={() => {}} 
+                                    drawerWidth={400}
+                                    locationCoords={{}}
+                                    mode="crawlBeingViewed"
+                                />
+                            ) : (
+                                <>
+                                    <Typography sx={styles.logo} variant="h5">
+                                    {crawl?.crawlName}
+                                    </Typography>
+                                    <Typography className="center-row" variant="body2" color="text.secondary" >
+                                    <CalendarMonthIcon sx={{mr: 1}} /> {formattedStartDate} - {formattedEndDate}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" className="center-row">
+                                    <GroupsIcon sx={{mr: 1}} /> {attendeesCount === 1 ? `${attendeesCount} person going` : `${attendeesCount} people going`}
+                                    </Typography>
+                                    <Typography className="intimacy center-row" variant="body2" color="text.secondary" >
+                                    {crawl?.intimacyLevel === 'Public' ? <PublicIcon sx={{mr: 1}} /> : <AdminPanelSettingsIcon sx={{mr: 1}} />}{crawl?.intimacyLevel}
+                                    </Typography>
+                                    <Divider sx={{mt: 2}} />
+                                    {crawl?.selectedBars.map((bar, index) => (
+                                        <BarCard key={index} bar={bar} mode="viewing" />
+                                    ))}
+                                </>
+                            )}
+                        </div>
+                        <div className='app-flex app-col app-relative app-overflow-hidden app-h-percent-100 app-fl-8 map-container' ref={mapContainerRef} />
+                    </>
+                ) : (
+                    <div className="app-flex app-col app-jc-center app-ai-center app-w-percent-100 no-access-container">
+                        <Typography sx={styles.logo} variant="h5">Who do you know here?</Typography>
+                        <Typography className="center-row" variant="body2" color="text.secondary" >You are not on the guest list of this private bar crawl.</Typography>
+                        <Box className="app-flex app-row app app-jc-between app-gap-1">
+                            <Button
+                                variant="contained"
+                                fullWidth
+                                sx={styles.actionButtonLight}
+                                onClick={handleButtonClick(`/Dashboard/${token}`, "Dashboard")}
+                            >
+                                Dashboard
+                            </Button>
+                            <Button
+                                variant="contained"
+                                fullWidth
+                                sx={styles.actionButtonDark}
+                                onClick={handleButtonClick("/", "Root")}
+                            >
+                                Create a Crawl
+                            </Button>
+                        </Box>
+                    </div>
+                )}
             </>
         ) : (
             <div className="app-flex app-col app-jc-center app-ai-center app-w-percent-100 no-access-container">
                 <Typography sx={styles.logo} variant="h5">Who do you know here?</Typography>
-                <Typography className="center-row" variant="body2" color="text.secondary" >You are not on the guest list of this private bar crawl.</Typography>
-                <Box className="app-flex app-row app app-jc-between w-300">
+                <Typography className="center-row" variant="body2" color="text.secondary" >You can only view bar crawl details when you are logged in.</Typography>
+                <Box className="app-flex app-row app app-jc-between app-gap-1">
                     <Button
-                    variant="contained"
-                    fullWidth
-                    sx={styles.actionButtonLight}
-                    >
-                        Dashboard
-                    </Button>
-                    <Button
-                    variant="contained"
-                    fullWidth
-                    sx={styles.actionButtonDark}
+                        variant="contained"
+                        fullWidth
+                        sx={styles.actionButtonLight}
+                        onClick={handleButtonClick("/", "Root")}
                     >
                         Create a Crawl
+                    </Button>
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        sx={styles.actionButtonDark}
+                        onClick={handleButtonClick("/Login", "Auth")}
+                    >
+                        Login
                     </Button>
                 </Box>
             </div>
