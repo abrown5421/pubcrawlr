55910fd6a7b7752767500ac0d9e45a405244cfb2
abrown5421 <augustashtonbrown@gmail.com>
2025-05-01 12:43:27 -0400

    trying a react wrapper for trianglify

--- src/components/TrianglifyBanner.tsx
+++ src/components/TrianglifyBanner.tsx
@@ -1,34 +1,28 @@
-import React, { useEffect, useRef } from 'react';
-import trianglify from 'trianglify'; 
+import React from 'react';
+import Trianglify from 'react-trianglify';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { TrianglifyBannerProps } from '../types/globalTypes';
 
 const TrianglifyBanner: React.FC<TrianglifyBannerProps> = ({ token = '' }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
   const { cellSize, variance, xColors, yColors } = useSelector(
     (state: RootState) => state.trianglify
   );
 
-  useEffect(() => {
-    const pattern = trianglify({
-      width: window.innerWidth,
-      height: 200,
-      cellSize,
-      variance,
-      seed: token,
-      xColors: xColors.length ? xColors : undefined,
-      yColors: yColors.length ? yColors : undefined,
-    });
-
-    if (ref.current) {
-      ref.current.innerHTML = '';
-      ref.current.appendChild(pattern.toCanvas());
-    }
-  }, [cellSize, variance, xColors, yColors, token]);
-
-  return <div ref={ref} className="banner-image" />;
+  return (
+    <div className="banner-image">
+      <Trianglify
+        width={window.innerWidth}
+        height={200}
+        cellSize={cellSize}
+        variance={variance}
+        seed={token}
+        xColors={xColors.length ? xColors : undefined}
+        yColors={yColors.length ? yColors : undefined}
+        output="canvas"
+      />
+    </div>
+  );
 };
 
 export default TrianglifyBanner;
