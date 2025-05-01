4348a4b3603a17620a6b44267186d0290e56b2ba
abrown5421 <augustashtonbrown@gmail.com>
2025-05-01 16:02:58 -0400

    including trianglify in the vite config optimize deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
   build: {
-    target: 'es2018', // Or 'es2015' if really needed
+    target: 'es2015',
     commonjsOptions: {
       include: [/node_modules/],
     },
   },
   optimizeDeps: {
     include: ['trianglify'],
+    esbuildOptions: {
+      target: 'es2015',
+    },
   },
 });
