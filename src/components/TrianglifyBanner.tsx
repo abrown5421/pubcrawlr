50e141a3762b6a141e9385f9b6f7dcc8371a2d52
abrown5421 <augustashtonbrown@gmail.com>
2025-05-01 12:49:13 -0400

    undoing the last fix

--- src/components/TrianglifyBanner.tsx
+++ src/components/TrianglifyBanner.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import Trianglify from 'react-trianglify';
+import React, { useEffect, useRef } from 'react';
+import trianglify from 'trianglify'; 
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { TrianglifyBannerProps } from '../types/globalTypes';
 
 const TrianglifyBanner: React.FC<TrianglifyBannerProps> = ({ token = '' }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
   const { cellSize, variance, xColors, yColors } = useSelector(
     (state: RootState) => state.trianglify
   );
 
-  return (
-    <div className="banner-image">
-      <Trianglify
-        width={window.innerWidth}
-        height={200}
-        cellSize={cellSize}
-        variance={variance}
-        seed={token}
-        xColors={xColors.length ? xColors : undefined}
-        yColors={yColors.length ? yColors : undefined}
-        output="canvas"
-      />
-    </div>
-  );
+  useEffect(() => {
+    const pattern = trianglify({
+      width: window.innerWidth,
+      height: 200,
+      cellSize,
+      variance,
+      seed: token,
+      xColors: xColors.length ? xColors : undefined,
+      yColors: yColors.length ? yColors : undefined,
+    });
+
+    if (ref.current) {
+      ref.current.innerHTML = '';
+      ref.current.appendChild(pattern.toCanvas());
+    }
+  }, [cellSize, variance, xColors, yColors, token]);
+
+  return <div ref={ref} className="banner-image" />;
 };
 
 export default TrianglifyBanner;
