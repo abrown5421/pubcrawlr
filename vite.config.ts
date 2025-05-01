f28150c038b1f392b525f4f60559ba6ab8ff4250
abrown5421 <augustashtonbrown@gmail.com>
2025-05-01 16:18:59 -0400

    reconfiguring the vite config again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015',
+    target: 'es2015', 
+    minify: 'esbuild', 
     commonjsOptions: {
       include: [/node_modules/],
     },
